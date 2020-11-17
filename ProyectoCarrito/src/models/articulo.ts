cb1f906d2b48e157fc6a137b63354ac52e0cba6b
Juan <chavezmarengo@hotmail.com>
2020-11-17 10:42:25 +0100

    comienzo

---
+++ ProyectoCarrito/src/models/articulo.ts
@@ -0,0 +1,19 @@
+class Articulo{
+
+    nombre:string;
+    descripcion:string;
+    color:string;
+    talla:number;
+    cantidad:number;
+    precio:number;
+
+    constructor(nombre,descripcion,color,talla,cantidad,precio) {
+    
+        this.nombre= nombre;
+        this.descripcion = descripcion;
+        this.color = color;
+        this.talla = talla;
+        this.cantidad = cantidad;
+        this.precio = precio;
+    }
+}

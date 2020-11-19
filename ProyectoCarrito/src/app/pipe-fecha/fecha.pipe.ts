9b84f629c8d4ca8f8f01d0e0ecf78469aeb027d5
Juan <chavezmarengo@hotmail.com>
2020-11-19 19:00:39 +0100

    version 2

---
+++ ProyectoCarrito/src/app/pipe-fecha/fecha.pipe.ts
@@ -0,0 +1,16 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'fecha'
+})
+export class FechaPipe implements PipeTransform {
+
+  transform(value: string): string {
+    var meses = new Array ("Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio","Agosto","Septiembre","Octubre","Noviembre","Diciembre");
+    var diasSemana = new Array("Domingo","Lunes","Martes","Miércoles","Jueves","Viernes","Sábado");
+    var f=new Date();
+    
+    return (diasSemana[f.getDay()] + ", " + f.getDate() + " de " + meses[f.getMonth()] + " de " + f.getFullYear());
+  }
+
+}
